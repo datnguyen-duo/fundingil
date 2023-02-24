6e3649a75c9fec8cb826be689d7b8eac3dcc6915
Nemanja <nemanjapavloviic@gmail.com>
2023-02-24 15:46:44 +0100

    deleted theme and reset git repository

--- 404.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<div class="page-404-container">
-
-</div>
-<?php get_footer(); ?>
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '6bbf5087c72d0ae17fd7');
